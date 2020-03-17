db2c60e2dc6a88d4bea11e4c5f44ff2ae866a736
unknown <minldft@gmail.com>
2020-03-17 13:29:00 +0300

    deploy

--- src/App.js
+++ src/App.js
@@ -88,7 +88,7 @@ class App extends Component {
       .then(response => response.json())
       .then(response => {
         if (response) {
-          fetch("https://pure-garden-07292.herokuapp.com:3000/image", {
+          fetch("https://pure-garden-07292.herokuapp.com/image", {
             method: "put",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({
